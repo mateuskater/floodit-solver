1b6444bc1d761af0f02665ae844f4158cafc36ed
Mateus <mateusk_10@hotmail.com>
2023-06-05 00:11:10 -0300

    adicionada logica de pintar tabuleiro

--- main.c
+++ main.c
@@ -2,15 +2,47 @@
 #include <stdlib.h>
 #include <strings.h>
 
+typedef struct t_map{
+    int nlinhas;
+    int ncolunas;
+    int cores;
+    int cor_atual;
+    int **map;
+}
+
+void paint(t_map *map, int l, int c, int cor){
+    
+    map->map[l][c] = cor;
+    if (l < map->nlinhas && map->map[l+1][c] == map->cor_atual)
+        paint(map, l+1, c, cor);
+    if (l < map->nlinhas && map->map[l-1][c] == map->cor_atual)
+        paint(map, l-1, c, cor);
+    if (c < map->ncolunas && map->map[l][c+1] == map->cor_atual)
+        paint(map, l, c+1, cor);
+    if (c < map->ncolunas && map->map[l][c-1] == map->cor_atual)
+        paint(map, l, c-1, cor);
+}
+int winCheck(t_map *map){
+    int cor = map->map[1][1];
+    for (int i = 0; i < map->ncolunas; i++){
+        for (int j = 0; j < map->nlinhas; j++){
+            if (map->map[i][j] != cor)
+                return 0;
+        }
+    }
+    return 1;
+}
+
 int main(){
+    t_map map;
     int cores;
     int linhas;
     int colunas;
-
+    int tentativas = 10;
     int teste;
-
+    int venceu = 0;
     int **tabuleiro;
-
+    
     scanf("%d %d %d\n", &linhas, &colunas, &cores);
     tabuleiro = (int**)malloc(linhas * sizeof(int*));
     for(int i = 0; i < linhas; i++) tabuleiro[i] = (int*)malloc(colunas * sizeof(int));
@@ -25,5 +57,14 @@ int main(){
             }
         }
     }
-    printf("feito\n");
+    // main game loop
+    for (int i = 0; i < tentativas; i++){
+       if (winCheck(map)){
+           venceu = 1;
+           break;
+       }
+       scanf("%d", &prox_cor);
+       paint(map, 0, 0, prox_cor); 
+    }
+    if venceu printf("venceste\n") else printf("perdeste\n");
 }
