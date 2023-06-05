03af18a339794f946e012b54d4a22d2e67530489
MATEUS KATER POMBEIRO <mkp19@inf.ufpr.br>
2023-06-05 18:33:50 -0300

    jogo basico funcionando

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
+#include <time.h>
 
 typedef struct t_map{
     int nlinhas;
@@ -8,20 +9,21 @@ typedef struct t_map{
     int cores;
     int cor_atual;
     int **map;
-}
+} t_map;
 
 void paint(t_map *map, int l, int c, int cor){
     
     map->map[l][c] = cor;
-    if (l < map->nlinhas && map->map[l+1][c] == map->cor_atual)
+    if (l < map->nlinhas-1 && map->map[l+1][c] == map->cor_atual)
         paint(map, l+1, c, cor);
-    if (l < map->nlinhas && map->map[l-1][c] == map->cor_atual)
+    if (l > 0 && map->map[l-1][c] == map->cor_atual)
         paint(map, l-1, c, cor);
-    if (c < map->ncolunas && map->map[l][c+1] == map->cor_atual)
-        paint(map, l, c+1, cor);
-    if (c < map->ncolunas && map->map[l][c-1] == map->cor_atual)
+    if (c > 0 && map->map[l][c-1] == map->cor_atual)
         paint(map, l, c-1, cor);
+    if (c < map->ncolunas-1 && map->map[l][c+1] == map->cor_atual)
+        paint(map, l, c+1, cor);
 }
+
 int winCheck(t_map *map){
     int cor = map->map[1][1];
     for (int i = 0; i < map->ncolunas; i++){
@@ -33,38 +35,84 @@ int winCheck(t_map *map){
     return 1;
 }
 
+t_map * gera_mapa(int linhas, int colunas, int cores){
+    t_map *new;
+
+    new = malloc(sizeof(t_map));
+
+    srand(time(NULL));
+    new->nlinhas = linhas;
+    new->ncolunas = colunas;
+    int **tabuleiro = (int**)malloc(linhas * sizeof(int*));
+    for(int i = 0; i < linhas; i++) tabuleiro[i] = (int*)malloc(colunas * sizeof(int));
+    new->map = tabuleiro;
+    for (int i = 0; i < linhas; i++){
+        for (int j = 0; j < colunas; j++){
+            new->map[i][j] = rand() % cores;
+        }
+    }
+    new->cor_atual = new->map[0][0];
+    return new;
+}
+
+void mostra_mapa(t_map *map){
+
+    for (int i = 0; i < map->nlinhas; i++){
+        for (int j = 0; j < map->ncolunas; j++){
+            printf("%d ", map->map[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
 int main(){
-    t_map map;
+    t_map *map;
     int cores;
     int linhas;
     int colunas;
-    int tentativas = 10;
+    int tentativas = 25;
     int teste;
     int venceu = 0;
     int **tabuleiro;
+    char canto = 'a';
+    int prox_cor;
+
+    // scanf("%d %d %d\n", &linhas, &colunas, &cores);
+
+    map = gera_mapa(20, 20, 6);
     
-    scanf("%d %d %d\n", &linhas, &colunas, &cores);
-    tabuleiro = (int**)malloc(linhas * sizeof(int*));
-    for(int i = 0; i < linhas; i++) tabuleiro[i] = (int*)malloc(colunas * sizeof(int));
-    for (int i = 0; i < linhas; i++){
-        for (int j = 0; j < colunas; j++){
-            scanf("%d ", &teste);
-            if (teste > cores){
-                printf("tabuleiro invalido. verifique os valores\n");
-                exit(1);
-            }else{
-                tabuleiro[i][j] = teste;
-            }
-        }
-    }
+    // for (int i = 0; i < linhas; i++){
+    //     for (int j = 0; j < colunas; j++){
+    //         scanf("%d ", &teste);
+    //         if (teste > cores){
+    //             printf("tabuleiro invalido. verifique os valores\n");
+    //             exit(1);
+    //         }else{
+    //             tabuleiro[i][j] = teste;
+    //         }
+    //     }
+    // }
     // main game loop
     for (int i = 0; i < tentativas; i++){
-       if (winCheck(map)){
-           venceu = 1;
-           break;
-       }
-       scanf("%d", &prox_cor);
-       paint(map, 0, 0, prox_cor); 
+        if (winCheck(map)){
+            venceu = 1;
+            break;
+        }
+        mostra_mapa(map);
+        scanf("%c %d", &canto, &prox_cor);
+        getc(stdin);
+        if (canto == 'a')
+            paint(map, 0, 0, prox_cor);
+        else if (canto == 'b')
+            paint(map, 0, map->ncolunas-1, prox_cor);
+        else if (canto == 'c')
+            paint(map, map->nlinhas-1, map->ncolunas-1, prox_cor);
+        else if (canto == 'd')
+            paint(map, map->nlinhas-1, 0, prox_cor);
+        
+        map->cor_atual = prox_cor;
+
     }
-    if venceu printf("venceste\n") else printf("perdeste\n");
+    venceu ? printf("venceste\n"): printf("perdeste\n");
 }
